0f654cc96544084ef415a83f42e85b3acd7d3a3d
Lalit sharma <geekymonkcp742k@gmail.com>
2023-07-13 15:11:14 +0530

    some cleanups

--- main.go
+++ main.go
@@ -10,18 +10,14 @@ import (
 )
 
 func main(){
-		scanner := bufio.NewScanner(os.Stdin) // create new scanner to read input from user (domain name) 
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Printf("domain,hasMX,hasSPF,spfRecords,hasDMARC,dmarcRecord\n")
 
-	fmt.Printf("domain,hasMX,hasSPF,spfRecords,hasDMARC,dmarcRecord\n") // prompt user to enter domain name
-
-	for scanner.Scan() { // loop through each line of input
-
+		for scanner.Scan() { 
 		checkDomain(scanner.Text()) // call checkDomain function to check domain name
 	}
-	if err := scanner.Err(); err != nil { // check for errors
-
-		log.Fatal("Couldn't read from input: %v\n",err) // log error
-
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Couldn't read from input: %v\n",err) 
 }
 }
 
@@ -29,24 +25,23 @@ func main(){
 
 func checkDomain(domain string) {
 
-		var hasMX,hasSPF,hasDMARC bool // create variables to store boolean values
-
-		var spfRecords,dmarcRecord string // create variables to store string values
+		var hasMX,hasSPF,hasDMARC bool 
+		var spfRecords,dmarcRecord string 
 
 
 		mxRecords, err := net.LookupMX(domain) // lookup MX records for domain name
 
 		if err != nil { // check for errors
-		log.Printf("Couldn't lookup MX records for %s: %v\n",domain,err) // log error
+		log.Printf("Couldn't lookup MX records for %s: %v\n",domain,err) 
 		
 		}
-		if len(mxRecords) > 0 { // check if MX records exist
-			hasMX = true // set hasMX to true
+		if len(mxRecords) > 0 { 
+			hasMX = true
 		}
 
 		txtRecords, err := net.LookupTXT(domain) // lookup SPF records for domain name
-		if err != nil { // check for errors
-			log.Printf("Couldn't lookup SPF records for %s: %v\n",domain,err) // log error
+		if err != nil { 
+			log.Printf("Couldn't lookup SPF records for %s: %v\n",domain,err)
 		}
     // loop through each SPF record
 		for _, record := range txtRecords { 
@@ -59,22 +54,20 @@ func checkDomain(domain string) {
 		}
 		// lookup DMARC record for domain name 
 		dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
-		if err != nil { // check for errors
-			log.Printf("Couldn't lookup DMARC records for %s: %v\n",domain,err) // log error
+		if err != nil { 
+			log.Printf("Couldn't lookup DMARC records for %s: %v\n",domain,err) 
 		}
 
 		// loop through each DMARC record
 	  for _ , record := range dmarcRecords { 
 			if strings.HasPrefix(record, "v=DMARC1") { // check if DMARC record exists
 		}
-			hasDMARC = true // set hasDMARC to true
-			dmarcRecord = record // set dmarcRecord to record
+			hasDMARC = true 
+			dmarcRecord = record 
 			break
 
 		}
 
 		fmt.Printf("%s,%t,%t,%s,%t,%s\n",domain,hasMX,hasSPF,spfRecords,hasDMARC,dmarcRecord) // print results to console
-
-
 
 }
